e9f2485570927e7115d045b060fb7b796d36e463
Arianne <github@shield.paintilya.dev>
2024-12-14 14:48:00 -0500

    Testing a fix for CI/CD (again)

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,11 @@ const nextConfig: NextConfig = {
   /* config options here */
   // For DEV
   //reactStrictMode: true
+  basePath: "/",
   output: "export",
+  images: {
+    unoptimized: true
+  }
   // basePath: "/ariannelafraise-website"
   // used this to fix CSS issue when website is on a subpath.
   // no longer needed right now, since im using a custom domain for github pages
