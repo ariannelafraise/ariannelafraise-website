8dbc0cea94e13200fa45203f67cf9dd84f79119e
Arianne <github@shield.paintilya.dev>
2024-12-15 02:46:16 -0500

    Responsive + font tweak

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   // For DEV
   //reactStrictMode: true
+
   basePath: "",
   output: "export",
   images: {
     unoptimized: true
   }
+  
   // basePath: "/ariannelafraise-website"
   // used this to fix CSS issue when website is on a subpath.
   // no longer needed right now, since im using a custom domain for github pages
