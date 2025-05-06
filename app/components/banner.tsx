90c3949b95c350ef177801a1824e122a89842091
ariannelafraise <github@shield.paintilya.dev>
2025-05-06 16:11:25 -0400

    Updated projects and fixed some CSS

--- app/components/banner.tsx
+++ app/components/banner.tsx
@@ -7,8 +7,8 @@ interface Props {
 const ProjectCard: React.FC<Props> = ({title, message, color}) => {
     return (
         <div className="border" style={{color: color, textAlign: "center", marginTop: "5vh" }}>
-            <h3>{title}</h3>
-            <p>{message}</p>
+            <h3 style={{color: color}}>{title}</h3>
+            <p style={{color: color}}>{message}</p>
         </div>
     );
 };
--- app/components/introduction.tsx
+++ app/components/introduction.tsx
@@ -4,8 +4,6 @@ const Introduction = () => {
     return (
         <div className='introduction'>
             <h1>Arianne</h1>
-            <p>ʕっ•ᴥ•ʔっ</p>
-            <p>software engineering student & ctf player</p>
             <p><a href='https://github.com/ariannelafraise'>Github</a>↗&nbsp;&nbsp;&nbsp;<a href='https://www.root-me.org/ariannelafraise'>Root-me</a>↗</p>
             <p></p>
         </div>
