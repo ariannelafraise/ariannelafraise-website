810a60995a606a2259191cc2cf1802bc8082dd66
ariannelafraise <76917463+ariannelafraise@users.noreply.github.com>
2026-01-10 22:08:11 -0500

    updated projects

--- app/page.tsx
+++ app/page.tsx
@@ -1,43 +1,40 @@
 import Introduction from "./components/introduction";
 import ProjectsList from "./components/projects-list";
-import Banner from './components/banner';
+import Banner from "./components/banner";
 import Footer from "./components/footer";
 import type { Project } from "./models/project";
-import { fetchProjects } from './lib/projectsUtils';
+import { fetchProjects } from "./lib/projectsUtils";
 
-const getProjects = () :  Array<Project> => {
+const getProjects = (): Array<Project> => {
   return fetchProjects();
-}
+};
 
 export default function Home() {
-
   const projects: Array<Project> = getProjects();
 
-  return(
+  return (
     <>
       {/*
-      <Banner 
+      <Banner
         title="/!\ AMBER ALERT /!\"
         message="all my strawberries have disappeared from my site. email: tip@federal-berry-of-investigation.gov with any tips, or contact me directly. thanks."
         color="#ff7575"/>
       */}
-      <Introduction/>
-      {
-        projects.length == 0
-        ?
+      <Introduction />
+      {projects.length == 0 ? (
         <div className="center-text">
           <p>...</p>
-          <h4>No projects to show</h4> 
+          <h4>No projects to show</h4>
         </div>
-        :
-        <ProjectsList projects={projects.map((p: Project) => p.data)}/>
-      }
-      {
+      ) : (
+        <ProjectsList projects={projects.map((p: Project) => p.data)} />
+      )}
+      {/*
         <>
           <Banner title="🚧 Art section planned 🚧" message="I make photo composites and want to learn music production" color="#ffc824ff"/>
         </>
-      }
-      <Footer/>
+      */}
+      <Footer />
     </>
   );
-};
+}
