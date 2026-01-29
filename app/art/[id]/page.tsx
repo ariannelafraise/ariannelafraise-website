1fda4f35a8f770b5e83a3818212aabe97c6477f0
ariannelafraise <76917463+ariannelafraise@users.noreply.github.com>
2026-01-28 21:51:58 -0500

    art section

---
+++ app/art/[id]/page.tsx
@@ -0,0 +1,36 @@
+import Footer from "../../components/footer";
+import Link from "next/link";
+
+import { fetchAllArt, fetchArt } from "@/app/lib/artUtils";
+
+import "./page.css";
+
+export async function generateStaticParams() {
+  const allArt: Array<string> = fetchAllArt();
+  return allArt.map((id: string) => ({
+    id: id,
+  }));
+}
+
+function getArt(id: string): string | undefined {
+  return fetchArt(id);
+}
+
+export default async function ArtImage({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) {
+  const artFile = getArt((await params).id);
+
+  return (
+    <>
+      <div className="content">
+        <Link href={"/media/art/" + artFile}>
+          <img src={"/media/art/" + artFile} alt="art image" width={500} />
+        </Link>
+      </div>
+      <Footer />
+    </>
+  );
+}
