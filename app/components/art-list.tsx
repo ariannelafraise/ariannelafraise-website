1ab242db6a1e87b8608a3ef7abe3d717e6f3cc51
ariannelafraise <76917463+ariannelafraise@users.noreply.github.com>
2026-01-28 21:54:27 -0500

    fix unused var

--- app/components/art-list.tsx
+++ app/components/art-list.tsx
@@ -11,7 +11,7 @@ const ArtList: React.FC<Props> = ({ arts }) => {
     <>
       <h1 className="center-text">My art</h1>
       <div className="art-list">
-        {arts.map((a: string, index: number) => (
+        {arts.map((a: string) => (
           <Link className="art-link" key={a} href={"/art/" + a}>
             <img className="art" src={"/media/art/" + a} alt="art image" />
           </Link>
